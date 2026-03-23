6ad501d6c86f4ca0c0321c1f37c399b269d4857f
Luna <lunatessarinipacheco@hotmail.com>
2026-03-23 08:02:51 -0300

    Meu primeiro repositório

---
+++ mvcAluno/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// GET - listar os usuários cadastrados
+Route::get('/alunos/listar', [AlunoController::class, 'listar'])-> name('aluno.listar');
+
+Route::get('/aluno/cadastrar', function(){
+    return view('cadastro');
+
+})->name('aluno.cadastro');
+
+Route::post('/aluno/salvar', [AlunoController::class, 'add'])->name('aluno.salvar');
